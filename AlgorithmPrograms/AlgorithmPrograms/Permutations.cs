6282203859a47c4b4a99b8116cb2a32b19dcfa5a
Reshma2310 <dreshma253@gmail.com>
2022-06-02 12:31:31 +0500

    [Reshma] - Permutations of a String

---
+++ AlgorithmPrograms/AlgorithmPrograms/Permutations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    internal class Permutations
+    {
+        public void Permute(String input, String output)
+        {
+            if (input.Length == 0)
+            {
+                Console.Write(output + "  ");
+                return;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char ap = input[i];
+                String left_substr = input.Substring(0, i);
+                String right_substr = input.Substring(i + 1);
+                String result = left_substr + right_substr;
+                Permute(result, output + ap);
+            }
+        }
+    }
+}
---
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace AlgorithamProblems
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            String input;
+            String output = "";
+
+            Console.Write("Enter the string: ");
+            input = Console.ReadLine();
+
+            Console.Write("\nAll possible strings are : ");
+            AlgorithmPrograms.Permutations prog = new AlgorithmPrograms.Permutations();
+            prog.Permute(input, output);
+        }
+    }
+}
