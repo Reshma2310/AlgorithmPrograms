028acfbbf79565ea3c3d910a8c0c19ff1e8c1426
Reshma2310 <dreshma253@gmail.com>
2022-06-02 14:33:15 +0500

    [Reshma] - Binary Search of Word from Word List

---
+++ AlgorithmPrograms/AlgorithmPrograms/BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class BinarySearch
+    {
+        public int binarySearch(String[] arr, String x)
+        {
+            int l = 0, r = arr.Length - 1;
+            while (l <= r)
+            {
+                int m = l + (r - l) / 2;
+
+                int res = x.CompareTo(arr[m]);                
+                if (res == 0)// Check if x is present at mid
+                    return m;                
+                if (res > 0)
+                    l = m + 1;// If x greater, ignore left half                
+                else
+                    r = m - 1;// If x is smaller, ignore right half
+            }
+            return -1;
+        }
+    }
+}
--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -5,15 +5,45 @@ namespace AlgorithamProblems
     {
         public static void Main(String[] args)
         {
-            String input;
-            String output = "";
+            Console.WriteLine("Select an Option\n1. Permutations of a String\n2. Binary Search\n3. End the Program");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    String input;
+                    String output = "";
 
-            Console.Write("Enter the string: ");
-            input = Console.ReadLine();
-
-            Console.Write("\nAll possible strings are : ");
-            AlgorithmPrograms.Permutations prog = new AlgorithmPrograms.Permutations();
-            prog.Permute(input, output);
+                    Console.Write("Enter the string: ");
+                    input = Console.ReadLine();
+                    Console.WriteLine("All possible strings are : ");
+                    AlgorithmPrograms.Permutations prog = new AlgorithmPrograms.Permutations();
+                    prog.Permute(input, output);
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the Number of words to be given as input");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    string[] arr = new string[n];
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        Console.WriteLine("Enter {0} words",n);
+                        arr = Console.ReadLine().Split(",");
+                        break;
+                    }
+                    Console.WriteLine("Enter the word you want to search");
+                    String x = Console.ReadLine();
+                    AlgorithmPrograms.BinarySearch search = new AlgorithmPrograms.BinarySearch();
+                    int result = search.binarySearch(arr, x);
+                    if (result == -1)
+                        Console.WriteLine("Word is not present");
+                    else
+                        Console.WriteLine("Word found at " + "index " + result);
+                    Console.WriteLine();
+                        break;
+                default:
+                    break;
+            }
+            
         }
     }
 }
